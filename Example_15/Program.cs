afcaa7fb666099a71059cb2aa3ef3eefe839cd19
nikita <tolstoguzov.nikita98@mail.ru>
2023-03-02 13:01:58 +0300

    Лекция 4

---
+++ Example_15/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+//string[,] table = new string[2, 5];
+
+// table[1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows ++)
+// {
+//     for(int colums = 0; colums < 5; colums ++)
+//     {
+//         Console.WriteLine($"--{table[rows, colums]}--");
+//     }
+// }
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
